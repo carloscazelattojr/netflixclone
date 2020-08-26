3298ed1f5bb946985bc15ee59f50a686c43c4413
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2020-08-25 23:22:57 -0300

    Finalização do projeto

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-NetFlix Clone React  
+# NetFlix Clone React 
+
+## Descrição do Projeto
+    Projeto com a finalidade de estudos do framework React, onde foi reproduzido somente a página principal do Netflix. Com fonte de informações (conteúdo), foi utilizado a API do site TheMoviedb.org.
+
+## TheMovieDB e API
+Site: [https://www.themoviedb.org](https://www.themoviedb.org) 
+API: [https://api.themoviedb.org/3 ](https://api.themoviedb.org/3) 
+
+
+## Tecnologias utilizadas
+ `React` `CSS`
+
+## Bibliotecas utilizadas
+ `@material-ui/core` `@material-ui/icons`
+
+ ## Instruções para testes
+    Clonar o repositório e executar o comando `yarn` para instalar as bibliotecas.
+    Depois execute o camando `npm start` para iniciar o React.
--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
   
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData ] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
   
   useEffect( ()=>{
     const loadAll = async () => {
@@ -23,25 +25,49 @@ export default () => {
       
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
       setFeaturedData(chosenInfo);
-
-
-
     }
     loadAll();
   },[] );
 
 
+  useEffect( ()=>{
+    const scrollListener = () => {
+      if (window.scrollY > 10 ){
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    
+    window.addEventListener('scroll', scrollListener);
+    
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  },[]);
+
+
   return(
     <div className="page">
-      
+      <Header black={blackHeader} />
       {featuredData && <FeaturedMovie item={featuredData}/>}
 
       <section className="lists">
         {movieList.map( (item, key)=>(
           <MovieRow key={key}  title = {item.title} items={item.items} />
         ))}
-
       </section>
+
+      <footer>
+        Feito com <span role ='img' aria-label="Hang-lose">🤘🏻</span> por CJr<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
+      {movieList.length <=0 && 
+        <div className="loading">
+          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="carregando" />
+        </div>
+      }
     </div >
   );
 }
